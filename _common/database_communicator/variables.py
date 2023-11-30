85017c49b2a94dd1494b99cf122ae7c60d411277
Artur Makowski <artmak1@st.amu.edu.pl>
2023-11-30 14:37:02 +0100

    [ML_MODEL] feat: migrate data preprocessing from .ipynb to .py

---
+++ _common/database_communicator/variables.py
@@ -0,0 +1,7 @@
+LOCATION_LIST = ['Grunwald', 'Górczyn', 'Ławica', 'Łazarz', 'Junikowo', 'Jeżyce', 
+                 'Podolany', 'Sołacz', 'Wilda', 'Dębiec', 'Nowe Miasto', 'Łacina', 
+                 'Rataje', 'Starołęka Mała', 'Stare Miasto', 'Naramowice', 
+                 'Piątkowo', 'Winogrady', 'Chartowo']
+
+FEAT_COLS = ['status', 'size', 'property_type', 'rooms', 'floor', 'year_built', 'property_condition', 'location']
+TARGET_COL = 'price'
---
+++ ml_model/src/data/data_preprocessing.py
@@ -0,0 +1,164 @@
+import sys
+
+sys.path.append("../../../_common/database_communicator/")
+from variables import LOCATION_LIST, FEAT_COLS, TARGET_COL
+from db_connector import DBConnector
+import pandas as pd
+import re
+from sklearn.model_selection import train_test_split
+
+
+class DataPreprocessor:
+    def __init__(self) -> None:
+        """Initialize DataPreprocessor class and load data from database"""
+        connector = DBConnector()
+        engine = connector.create_sql_engine()
+
+        self.df = pd.read_sql_query("SELECT * FROM data_staging", con=engine)
+
+    def _handle_missing_and_duplicated_values(self):
+        """Handle missing and duplicated values in the dataset"""
+        self.df["floor"].fillna("brak informacji", inplace=True)
+        self.df["status"].fillna("brak informacji", inplace=True)
+        self.df["property_type"].fillna("brak informacji", inplace=True)
+        self.df["rooms"].fillna(1, inplace=True)
+        self.df["year_built"].fillna("brak informacji", inplace=True)
+        self.df["property_condition"].fillna("brak informacji", inplace=True)
+
+        self.df.drop_duplicates(inplace=True)
+
+        self.df = self.df[self.df["price"].notna()]
+        self.df = self.df[self.df["size"].notna()]
+
+    def _process_price(self, filter_price: float = 0.0):
+        """Process price column and filter out prices higher than filter_price"""
+        self.df["price"] = (
+            self.df["price"]
+            .str.replace("zł", "")
+            .str.replace(" ", "")
+            .replace(",", ".", regex=True)
+            .replace("Zapytajocenę", None, regex=True)
+            .astype(float)
+        )
+
+        if filter_price > 0.0:
+            self.df = self.df[self.df["price"] < filter_price]
+
+    def _process_size(self):
+        """Process size column"""
+        self.df["size"] = self.df["size"].str.replace(",", ".").astype(float)
+
+    def _process_location(self):
+        """Process location column"""
+
+        self.df["location"] = self.df["location"].apply(
+            lambda x: next(
+                (loc for loc in LOCATION_LIST if bool(re.search(loc, x))), "Poznań"
+            )
+        )
+
+    def _process_floor(self):
+        """Process floor column"""
+
+        self.df["floor"] = self.df["floor"].apply(
+            lambda x: x.split("/")[0] if type(x) == str else x
+        )
+
+        def extract_numbers(s):
+            return "".join(filter(str.isdigit, s)) if any(map(str.isdigit, s)) else s
+
+        self.df["floor"] = (
+            self.df["floor"]
+            .str.replace("parter", "0")
+            .str.replace("poddasze", "10")
+            .apply(extract_numbers)
+        )
+        self.df["floor"] = (
+            self.df["floor"]
+            .str.replace("zapytaj", "brak informacji")
+            .str.replace("suterena", "-1")
+        )
+
+    def _process_property_type(self):
+        """Process property_type column"""
+
+        self.df["property_type"] = (
+            self.df["property_type"]
+            .str.replace("plomba", "pozostałe")
+            .str.replace("bliźniak", "wolnostojący")
+            .str.replace("dom wolnostojący", "wolnostojący")
+        )
+
+    def _process_property_condition(self):
+        """Process property_condition column"""
+
+        self.df["property_condition"] = self.df["property_condition"].str.replace(
+            "zapytaj", "brak informacji"
+        )
+
+    def _cast_types(self):
+        numerical_col = ["size", "rooms"]
+        categorical_col = [
+            "status",
+            "property_type",
+            "floor",
+            "year_built",
+            "property_condition",
+            "location",
+        ]
+
+        for col in numerical_col:
+            self.df[col] = self.df[col].astype(float)
+
+        for col in categorical_col:
+            self.df[col] = self.df[col].astype("category")
+
+    def _select_features(self):
+        """Select features to be used in the model"""
+        self.df = self.df[FEAT_COLS + [TARGET_COL]]
+
+    def run_preprocessing_pipeline(self):
+        """Run preprocessing pipeline
+
+        Returns:
+            pd.DataFrame : preprocessed data
+        """
+
+        self._process_price()
+        self._handle_missing_and_duplicated_values()
+        self._process_size()
+        self._process_location()
+        self._process_floor()
+        self._process_property_type()
+        self._process_property_condition()
+        self._cast_types()
+        self._select_features()
+
+    def train_test_split(
+        self
+    ) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+        """Split data into train and test set"""
+        X_train, X_test, y_train, y_test = train_test_split(
+            self.df[FEAT_COLS],
+            self.df[TARGET_COL],
+            test_size=0.2,
+            random_state=42,
+            shuffle=True,
+        )
+        return X_train, X_test, y_train, y_test
+
+    def get(self):
+        """Return data
+
+        Returns:
+            pd.DataFrame : data
+        """
+        return self.df
+
+
+if __name__ == "__main__":
+    preprocessor = DataPreprocessor()
+
+    preprocessor.run_preprocessing_pipeline()
+    X_train, X_test, y_train, y_test = preprocessor.train_test_split()
+    df = preprocessor.get()
